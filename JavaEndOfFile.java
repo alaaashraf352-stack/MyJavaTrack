d10f07ee14825af0d1e601221353e69ccfc10ca3
alaaashraf352-stack <alaaashraf352@gmail.com>
2026-04-19 18:27:55 +0200

    Add files via upload
    Add 5 more Java HackerRank solutions

---
+++ JavaEndOfFile.java
@@ -0,0 +1,20 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaEndOfFile {
+    public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner input = new Scanner(System.in);
+            int i = 1;
+            while (input.hasNext()) ;
+            {
+                System.out.println(i + " " + input.nextLine());i++;
+            }
+            input.close();
+
+
+        }
+    }
+
+
---
+++ JavaLoop2.java
@@ -0,0 +1,33 @@
+package HackerRank;
+
+
+import java.util.Scanner;
+
+public class JavaLoop2 {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int result =a;
+            for(int x=0;x<n;x++){
+                result+=b*(int)Math.pow(2,x);
+                System.out.print(result + " ");
+
+
+            }
+            System.out.println();
+
+
+        }
+
+        in.close();
+
+    }
+
+
+
+    }
+
